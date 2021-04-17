9eade3b44456abd34c7fc97f44fb932b82f760b3
Claire Phillips <claireacam@gmail.com>
2021-04-17 13:49:51 -0400

    index file

--- models/index.js
+++ models/index.js
@@ -2,24 +2,23 @@ const Location = require('./Location');
 const Traveller = require('./Traveller');
 const Trips = require('./Trips');
 
-Traveller.hasMany(LibraryCard, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+Traveller.hasMany(Location, {
+  foreignKey: 'traveller_id',
 });
 
-// A reader can have many books
-Reader.hasMany(Book, {
-  foreignKey: 'reader_id',
-  onDelete: 'CASCADE',
+Location.hasMany(Traveller, {
+  foreignKey: 'location_id',
 });
 
-// A book belongs to a single reader
-Book.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+Location.belongsToMany(Traveller, {
+    through: 'Trips',
+    foreignKey: 'traveller_id'
+
 });
 
-LibraryCard.belongsTo(Reader, {
-  foreignKey: 'reader_id',
+Traveller.belongsToMany(Location, {
+  through: 'Trips',
+  foreignKey: 'location_id'
 });
 
 module.exports = { Location, Traveller, Trips };
