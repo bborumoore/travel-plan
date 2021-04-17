e4d36a45dc7c7ded6f4dde74b2596ee472c41090
bborumoore <bborumoore@gmail.com>
2021-04-17 13:14:45 -0400

    Made some edits to the file structure, set up basic server files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
